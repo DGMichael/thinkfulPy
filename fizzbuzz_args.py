96b7e218c492d566e178d860822e1c289b9670f4
Drew Michael <drew.g.michael@gmail.com>
2014-10-01 21:51:10 -0500

    Refactored.

--- fizzbuzz_args.py
+++ fizzbuzz_args.py
@@ -12,11 +12,11 @@ def fizzbuzz_args(arg_input):
 	else:
 		while True:
 			try:
-				num = int(raw_input("How far should we count up to? ")) + 1
+				num = int(raw_input("How far should we count up to? "))
 				break
 			except ValueError:
 				print "Please enter an integer value."
-	for i in range(1, int(num)):
+	for i in range(1, num + 1):
 		if i % 3 == 0 and i % 5 == 0:
 			print "Fizz Buzz"
 		elif i % 3 == 0:
@@ -31,13 +31,13 @@ def main():
 	if len(sys.argv) > 2:
 		sys.exit("Use: python ~/path/fizzbuzz_args.py <maximum integer>")
 	arg_input = 0
-	if len(sys.argv) > 1:
+	elif len(sys.argv) > 1:
 		if sys.argv[1].isdigit():
-			arg_input = int(sys.argv[1]) + 1
+			arg_input = int(sys.argv[1])
 		else:
 			while arg_input < 1:
 				try:
-					arg_input = int(raw_input("Please enter an interger value to count up to: ")) + 1
+					arg_input = int(raw_input("Please enter an interger value to count up to: "))
 					break
 				except ValueError:
 					print "Non-integer value entered...sorry play fair."
